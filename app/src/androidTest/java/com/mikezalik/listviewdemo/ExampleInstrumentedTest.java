c17a585476061f117545ed6da4cc543809a70423
mikezalik <mike.zalik@gmail.com>
2020-01-28 16:15:02 -0800

    Adds listView component in XML and MainActivity.

--- app/src/androidTest/java/com/mikezalik/listviewdemo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/mikezalik/listviewdemo/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.mikezalik.listviewdemo;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
--- app/src/main/java/com/mikezalik/listviewdemo/MainActivity.java
+++ app/src/main/java/com/mikezalik/listviewdemo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.mikezalik.listviewdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView myListView = findViewById(R.id.myListView);
+
+        
     }
 }
