6d507ebc95a3df34c78b3a3d26cfa4fbd74facbb
mikezalik <mike.zalik@gmail.com>
2020-01-28 16:41:27 -0800

    Adds toast message that displays the individual selected from the list.

--- app/src/main/java/com/mikezalik/listviewdemo/MainActivity.java
+++ app/src/main/java/com/mikezalik/listviewdemo/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -35,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Log.i("Person Selected", myFamily.get(i));
+                Toast.makeText(MainActivity.this, "The person you select is " + myFamily.get(i), Toast.LENGTH_SHORT).show();
             }
         });
     }
